ecb7a85aaf2c352f1c54d3a0ebcf3e868929815e
Sandeep <sandeepkumarp@users.noreply.github.com>
2018-11-30 22:26:53 +0530

    WIP : Melee Gameplay Ability with Cooldown
    #Enabled Module in Build.cs
    #Implemented Component and AbilitySystemInterface
    #Called Montage in Melee Ability instead.
    #Added Ability Cooldown as a GameplayEffect.

--- AbilitySystem/Source/AbilitySystem/Private/CharacterBase.cpp
+++ AbilitySystem/Source/AbilitySystem/Private/CharacterBase.cpp
@@ -1,33 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "CharacterBase.h"
+#include <AbilitySystemComponent.h>
+#include <GameplayAbility.h>
 
-// Sets default values
 ACharacterBase::ACharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComp"));
 
 }
 
-// Called when the game starts or when spawned
 void ACharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
-// Called every frame
 void ACharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
 
+void ACharacterBase::AcquireAbility(TSubclassOf<UGameplayAbility> AbilityToAcquire)
+{
+	if (AbilitySystemComp)
+	{
+		if (HasAuthority() && AbilityToAcquire)
+		{
+			FGameplayAbilitySpecDef SpecDef = FGameplayAbilitySpecDef();
+			SpecDef.Ability = AbilityToAcquire;
+
+			FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(SpecDef, 1);
+			AbilitySystemComp->GiveAbility(AbilitySpec);
+		}
+		AbilitySystemComp->InitAbilityActorInfo(this, this);
+	}
+}
+
+UAbilitySystemComponent* ACharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComp;
+}
--- AbilitySystem/Source/AbilitySystem/Public/CharacterBase.h
+++ AbilitySystem/Source/AbilitySystem/Public/CharacterBase.h
@@ -4,26 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include <AbilitySystemInterface.h>
 #include "CharacterBase.generated.h"
 
+class UAbilitySystemComponent;
+class UGameplayAbility;
+
 UCLASS()
-class ABILITYSYSTEM_API ACharacterBase : public ACharacter
+class ABILITYSYSTEM_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ACharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+	UAbilitySystemComponent* AbilitySystemComp;
+
+	UFUNCTION(BlueprintCallable, Category = "CharacterBase")
+	void AcquireAbility(TSubclassOf<UGameplayAbility> AbilityToAcquire);
+
+private:
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 };
